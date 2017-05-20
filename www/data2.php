b7cc9c3418271baeb5e9e01974f19ef92c9edce0
simon177 <szymon.banowski@gmail.com>
2017-05-20 15:26:52 +0200

    commit2

--- www/data2.php
+++ www/data2.php
@@ -1,43 +1,44 @@
 <?php
 $APIkey='&APIkey=fe753bc888bb6a75490621597fc3f4f93184ef9a2ffb5cffab3be87813b7023f';
 $link = $_POST['link'];
+ 
 $curl_options = array(
 	CURLOPT_URL => $link . $APIkey,
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_HEADER => false,
-	CURLOPT_TIMEOUT => 30,
-	CURLOPT_CONNECTTIMEOUT => 5
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HEADER => false,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_CONNECTTIMEOUT => 5
 );                              
  
 $curl = curl_init();
 curl_setopt_array( $curl, $curl_options );
 $result = curl_exec( $curl );
-
+ 
 $dataArray = array();
 $newOddArray = array();
 $newDataArray = array();
 $dataArray = json_decode($result, TRUE);
-
-
+ 
+ 
 foreach ($dataArray as &$value) {
     if (!in_array($value['odd_bookmakers'], $newOddArray)) {
-    	array_push($newOddArray, $value['odd_bookmakers']);
-	}
+        array_push($newOddArray, $value['odd_bookmakers']);
+    }
 }
-
+ 
 $dataArray = array_reverse($dataArray);
 foreach ($dataArray as &$value) {
-	if (in_array($value['odd_bookmakers'], $newOddArray)) {
-		$pos = array_search($value['odd_bookmakers'], $newOddArray);
-		unset($newOddArray[$pos]);
-
-    	array_push($newDataArray, $value);
-	}
+    if (in_array($value['odd_bookmakers'], $newOddArray)) {
+        $pos = array_search($value['odd_bookmakers'], $newOddArray);
+        unset($newOddArray[$pos]);
+ 
+        array_push($newDataArray, $value);
+    }
 }
-
-
-
+ 
+ 
+ 
 $newDataArray = array_reverse($newDataArray);
 $newDataArray = json_encode($newDataArray);
-
+ 
 echo $newDataArray;
